8919679d87a9f8b2d99320eb705151365521c223
Folau Kaveinga <folaukaveinga@gmail.com>
2025-12-10 22:57:26 -0700

    setup

---
+++ src/main/java/com/folautech/restclient/SpringbootWithRestclientApplication.java
@@ -0,0 +1,13 @@
+package com.folautech.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithRestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithRestclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folautech/restclient/SpringbootWithRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
